3d11b8d62f05975372b753d39efe1b914a20c798
FritzDavid <fritz.david94@googlemail.com>
2020-05-04 11:49:56 +0200

    added api_key variable so we just have to change on one point in the code; deleted semicolon

--- WeatherWrapper.py
+++ WeatherWrapper.py
@@ -1,6 +1,7 @@
-import pyowm;
+import pyowm
+api_key = 'f9129773efcfc84c3e2c1bf63eb2f65c'
+owm = pyowm.OWM(api_key)  # API Key
 
-owm = pyowm.OWM('f9129773efcfc84c3e2c1bf63eb2f65c')  # API Key
 
 observation = owm.weather_at_place('Karlsruhe, Ger')
 weather = observation.get_weather()
@@ -9,5 +10,3 @@ print('Wetter: ')
 print(weather)
 print('Temp:')
 print(temperature)
-
-
