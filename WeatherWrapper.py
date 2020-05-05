64611e13e191910dcdc771467794fbacbd273b82
FritzDavid <fritz.david94@googlemail.com>
2020-05-05 10:48:05 +0200

    user input for location and added a language option

--- WeatherWrapper.py
+++ WeatherWrapper.py
@@ -2,12 +2,17 @@ import pyowm
 import requests
 
 api_key = 'f9129773efcfc84c3e2c1bf63eb2f65c'  # API Key
-location = 'karlsruhe'
 owm = pyowm.OWM(api_key)
+
+
+#location = 'karlsruhe'
+location = input("Enter location:")
+print("location is: " + location)
+language = 'en'
 
 # Testing with API Call
 response = requests.get(
-    'http://api.openweathermap.org/data/2.5/forecast?q=karlsruhe&appid=f9129773efcfc84c3e2c1bf63eb2f65c')
+   'http://api.openweathermap.org/data/2.5/forecast?q=' + location + '&appid=' + api_key + '&lang=' + language)
 print(response.status_code)
 print(response.content)
 print('########')
