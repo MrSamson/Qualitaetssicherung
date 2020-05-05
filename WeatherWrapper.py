6b7bd2c66f357e911cb16db775adcbaa044ea54d
FritzDavid <fritz.david94@googlemail.com>
2020-05-05 11:39:41 +0200

    changing url call to make it more dynamic

--- WeatherWrapper.py
+++ WeatherWrapper.py
@@ -3,18 +3,17 @@ import requests
 
 api_key = 'f9129773efcfc84c3e2c1bf63eb2f65c'  # API Key
 owm = pyowm.OWM(api_key)
-
-
-#location = 'karlsruhe'
 location = input("Enter location:")
 print("location is: " + location)
 language = 'en'
 
 # Testing with API Call
-response = requests.get(
-   'http://api.openweathermap.org/data/2.5/forecast?q=' + location + '&appid=' + api_key + '&lang=' + language)
-print(response.status_code)
-print(response.content)
+url = 'http://api.openweathermap.org/data/2.5/forecast?q={}&appid={}&lang={}'.format(location, api_key, language)
+res = requests.get(url)
+data = res.json()
+
+print(res)
+print(data)
 print('########')
 
 # Testing with API functions
