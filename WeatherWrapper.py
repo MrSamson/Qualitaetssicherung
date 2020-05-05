fb66471379953af8d1b3cf37363ac43bf41b89d1
FritzDavid <fritz.david94@googlemail.com>
2020-05-05 10:34:09 +0200

    testing-purpose: added new request libary to get json format from api call

--- WeatherWrapper.py
+++ WeatherWrapper.py
@@ -1,8 +1,18 @@
 import pyowm
-api_key = 'f9129773efcfc84c3e2c1bf63eb2f65c'
-owm = pyowm.OWM(api_key)  # API Key
+import requests
 
+api_key = 'f9129773efcfc84c3e2c1bf63eb2f65c'  # API Key
+location = 'karlsruhe'
+owm = pyowm.OWM(api_key)
 
+# Testing with API Call
+response = requests.get(
+    'http://api.openweathermap.org/data/2.5/forecast?q=karlsruhe&appid=f9129773efcfc84c3e2c1bf63eb2f65c')
+print(response.status_code)
+print(response.content)
+print('########')
+
+# Testing with API functions
 observation = owm.weather_at_place('Karlsruhe, Ger')
 weather = observation.get_weather()
 temperature = weather.get_temperature('celsius')
