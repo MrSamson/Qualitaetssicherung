a27bd53fa35b05e01cd9c6bb678f9296d6226315
FritzDavid <fritz.david94@googlemail.com>
2020-05-06 19:01:16 +0200

    added function for apicalls

--- WeatherWrapper.py
+++ WeatherWrapper.py
@@ -1,26 +1,49 @@
 import pyowm
 import requests
+import Weather
+import json
 
-api_key = 'f9129773efcfc84c3e2c1bf63eb2f65c'  # API Key
-owm = pyowm.OWM(api_key)
-location = input("Enter location:")
-print("location is: " + location)
-language = 'en'
 
-# Testing with API Call
-url = 'http://api.openweathermap.org/data/2.5/forecast?q={}&appid={}&lang={}'.format(location, api_key, language)
-res = requests.get(url)
-data = res.json()
+class WeatherWrapper(object):
 
-print(res)
-print(data)
-print('########')
 
-# Testing with API functions
-observation = owm.weather_at_place('Karlsruhe, Ger')
-weather = observation.get_weather()
-temperature = weather.get_temperature('celsius')
-print('Wetter: ')
-print(weather)
-print('Temp:')
-print(temperature)
+
+
+    def __apicall(self, location):
+        # Testing with API Call
+        url = 'http://api.openweathermap.org/data/2.5/forecast?q={}&appid={}'.format(location, 'f9129773efcfc84c3e2c1bf63eb2f65c')
+        res = requests.get(url)
+        data = res.json()
+        return data
+
+        #print(res)
+
+    def publicapicall(self, location):
+        url = 'http://api.openweathermap.org/data/2.5/forecast?q={}&units={}&appid={}'.format(location,'metric', 'f9129773efcfc84c3e2c1bf63eb2f65c',)
+        res = requests.get(url)
+        data = res.json()
+        print(data)
+
+    def getCurrentWeather(self, location):
+
+        json = self.__apicall(location)
+
+        #return json
+        # todo: json auseinanderpfrimeln
+        feelsLike = (json['list'][0]['main']['feels_like'])
+        print(feelsLike)
+
+
+        # todo: create object with values from json
+        #weather = Weather()
+
+
+
+
+w = WeatherWrapper()
+w.getCurrentWeather('Karlsruhe')
+#print(w.getCurrentWeather('Karlsruhe'))
+
+
+
+
