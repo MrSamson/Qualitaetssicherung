428f432265f422ffed391ffb8607e3ec737ce0c3
FritzDavid <fritz.david94@googlemail.com>
2020-05-08 09:54:37 +0200

    add error exception

--- WeatherWrapper.py
+++ WeatherWrapper.py
@@ -4,24 +4,23 @@ from Weather import Weather
 import json
 import datetime
 import time
+import requests.exceptions
 
 
 class WeatherWrapper(object):
-    dict = {}
 
     def __apicall(self, location):
-        # Testing with API Call
+        api_key = 'f9129773efcfc84c3e2c1bf63eb2f65c'
         url = 'http://api.openweathermap.org/data/2.5/forecast?q={}&units={}&appid={}'.format(location, 'metric',
-                                                                                              'f9129773efcfc84c3e2c1bf63eb2f65c', )
+                                                                                              api_key, )
         res = requests.get(url)
-        data = res.json()
-        return data
+        try:
+            res.raise_for_status()
+            data = res.json()
+            return data
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
 
-    def publicapicall(self, location):
-        json_data = self.__apicall(location)
-        # currentWeather = json['list'][0]['dt']
-        currentWeather = json.loads(json_data)
-        return currentWeather
 
     def getCurrentWeather(self, location):
         json = self.__apicall(location)
@@ -40,15 +39,15 @@ class WeatherWrapper(object):
 
         return weatherlist[objName]
 
+
     def getTodayWeather(self, location):
         json = self.__apicall(location)
 
         # Object format: temperature, feelsLike, minTemp, maxTemp, dt, weatherState = ''
         weatherlist = {}
-        today = datetime.date.today()
         tomorrow = datetime.date.today() + datetime.timedelta(days=1)
         unixtime = time.mktime(tomorrow.timetuple())
-        # while dt_text <= heute
+
         i = 0
         while float(json['list'][i]['dt']) <= unixtime:
             objName = str(json['list'][i]['dt'])
@@ -63,30 +62,24 @@ class WeatherWrapper(object):
 
         return weatherlist
 
-    def getFiveDayForecast(self, location):
+
+    def getFiveDaysForecast(self, location):
         json = self.__apicall(location)
 
         # Object format: temperature, feelsLike, minTemp, maxTemp, dt, weatherState = ''
         weatherlist = {}
-        today = datetime.date.today()
-        fiveDaysLater = datetime.date.today() + datetime.timedelta(days=4)
-        unixtime = time.mktime(fiveDaysLater.timetuple())
-        i = 0
-        # while float(json['list'][i]['dt']) <= unixtime:
-        #     objName = str(json['list'][i]['dt'])
-        #     temperature = json['list'][i]['main']['temp']
-        #     temp_min = json['list'][i]['main']['temp_min']
-        #     temp_max = json['list'][i]['main']['temp_max']
-        #     temp_feels_like = json['list'][i]['main']['feels_like']
-        #     temp_dt = json['list'][i]['dt_txt']
-        #     weatherstate = json['list'][i]['weather'][0]['description']
-        #     weatherlist[objName] = Weather(temperature, temp_feels_like, temp_min, temp_max, temp_dt, weatherstate)
-        #     i += 1
 
-        # return weatherlist
-        return unixtime
+        for i in range(json["cnt"]):
+            objName = str(json['list'][i]['dt'])
+            temperature = json['list'][i]['main']['temp']
+            temp_min = json['list'][i]['main']['temp_min']
+            temp_max = json['list'][i]['main']['temp_max']
+            temp_feels_like = json['list'][i]['main']['feels_like']
+            temp_dt = json['list'][i]['dt_txt']
+            weatherstate = json['list'][i]['weather'][0]['description']
+            weatherlist[objName] = Weather(temperature, temp_feels_like, temp_min, temp_max, temp_dt, weatherstate)
 
-
+        return weatherlist
 
 
     # Testausgabe
@@ -94,7 +87,6 @@ class WeatherWrapper(object):
         wrapper = WeatherWrapper()
 
         list = wrapper.getTodayWeather(location)
-        # wrapper.getTodayWeather('Karlsruhe')
         for x in list:
             print("Uhrzeit: ")
             print(list[x].getDt())
@@ -108,21 +100,24 @@ class WeatherWrapper(object):
             print(list[x].getMaxTemp())
             print("------")
 
+
     def printforecast(self, location):
         wrapper = WeatherWrapper()
+        list = wrapper.getFiveDaysForecast(location)
+        for x in list:
+            print("Datum: ")
+            print(list[x].getDt())
+            print("temperatur: ")
+            print(wrapper.getCurrentWeather(location).getTemperature())
+            print("temp_min: ")
+            print(wrapper.getCurrentWeather(location).getMinTemp())
+            print("temp_max: ")
+            print(wrapper.getCurrentWeather(location).getMaxTemp())
+            print("feels like: ")
+            print(wrapper.getCurrentWeather(location).getFeelsLike())
+            print("Wetter: ")
+            print(wrapper.getCurrentWeather(location).getWeatherState())
 
-        print("Datum: ")
-        print(wrapper.getFiveDayForecast(location))
-        # print("temperatur: ")
-        # print(wrapper.getCurrentWeather(location).getTemperature())
-        # print("temp_min: ")
-        # print(wrapper.getCurrentWeather(location).getMinTemp())
-        # print("temp_max: ")
-        # print(wrapper.getCurrentWeather(location).getMaxTemp())
-        # print("feels like: ")
-        # print(wrapper.getCurrentWeather(location).getFeelsLike())
-        # print("Wetter: ")
-        # print(wrapper.getCurrentWeather(location).getWeatherState())
 
     def printCurrent(self, location):
         wrapper = WeatherWrapper()
@@ -142,8 +137,9 @@ class WeatherWrapper(object):
 
 
 wrapper = WeatherWrapper()
-# wrapper.printCurrent('Karlsruhe')
-# print("-----------------------------------")
-# wrapper.printToday('Karlsruhe')
-# print("-----------------------------------")
-wrapper.printforecast('Karlsruhe')
+wrapper.printCurrent('Karlsruhe')
+print("programm läuft weiter")
+print("-----------------------------------")
+wrapper.printToday('Karlsruhe')
+print("-----------------------------------")
+# wrapper.printforecast('Karlsruhe')
