ca76a47439e3fd97dba621311b901de239e2aa17
Andreas Deiß <andideiss@hotmail.de>
2020-05-31 18:11:20 +0200

    fixed testWeatherReport after we changed the methods in WeatherReport.py

--- testclasses/testWeatherReport.py
+++ testclasses/testWeatherReport.py
@@ -1,15 +1,14 @@
-import datetime
 import unittest
 from WeatherReport import WeatherReport
 
 
 # refering to: https://docs.python.org/3/library/unittest.html
 # basic unit tests for weather class
-class testWeatherWrapper(unittest.TestCase):
+class testWeatherReport(unittest.TestCase):
 
-    # checks if an object of WeatherWrapper is created
+    # checks if an object of WeatherReport is created
     # may be used for learning tests, to see how the constructor works
-    def testWeatherWrapperConstructor(self):
+    def testWeatherReportConstructor(self):
         w = WeatherReport()
         self.assertIsInstance(w, WeatherReport)
 
@@ -18,16 +17,16 @@ class testWeatherWrapper(unittest.TestCase):
     # may be used for learning tests
     def testGetCurrentWeatherKarlsruhe(self):
         location = 'Karlsruhe'
-        weatherWrapper = WeatherReport()
-        self.assertNotIsInstance(weatherWrapper.getCurrentWeather(location), WeatherReport)
+        weatherReport = WeatherReport()
+        self.assertNotIsInstance(weatherReport.getCurrentWeather(location), WeatherReport)
 
     # gets the current weather for Heidelberg and puts the results into an object
     # checks if all needed values are set
     # may be used for learning tests to see what values we can get
     def testGetCurrentWeatherKarlsruheCheckValues(self):
         location = 'Heidelberg'
-        weatherWrapper = WeatherReport()
-        currentWeather = weatherWrapper.getCurrentWeather(location)
+        weatherReport = WeatherReport()
+        currentWeather = weatherReport.getCurrentWeather(location)
         self.assertIsNotNone(currentWeather.getMinTemp())
         self.assertIsNotNone(currentWeather.getFeelsLike())
         self.assertIsNotNone(currentWeather.getMinTemp())
@@ -38,21 +37,21 @@ class testWeatherWrapper(unittest.TestCase):
     # gets the five day weather forecast for Mannheim and puts the results into a list
     # to check if there is more than one object delivered
     # may be used for learning tests
-    def testGetCurrentWeatherKarlsruhe(self):
+    def testGetCurrentWeatherKarlsruheCheckListSize(self):
         location = 'Heidelberg'
-        weatherWrapper = WeatherReport()
-        forecastList = weatherWrapper.getFiveDaysForecast(location)
+        weatherReport = WeatherReport()
+        forecastList = weatherReport.getFiveDaysForecast(location)
 
         self.assertGreater(len(forecastList), 1)
-        self.assertEqual(len(forecastList), 40)
+        self.assertEqual(len(forecastList), 5)
 
     # gets the five day weather forecast for Mannheim and puts the results into a list
     # checks if all values in all objects are set
     # may be used for learning tests
     def testGetCurrentWeatherHeidelbergCheckValues(self):
         location = 'Heidelberg'
-        weatherWrapper = WeatherReport()
-        forecastList = weatherWrapper.getFiveDaysForecast(location)
+        weatherReport = WeatherReport()
+        forecastList = weatherReport.getFiveDaysForecast(location)
 
         for i in forecastList:
             self.assertIsNotNone(forecastList[i].getMinTemp())
@@ -65,12 +64,7 @@ class testWeatherWrapper(unittest.TestCase):
     # todo: fix this
     # gets todays weather and checks if the date is really at least today
     def testGetTodayWeatherSchwetzingen(self):
-        location = 'Schwetzingen'
-        yesterday = datetime.date.today() - datetime.timedelta(days=1)
-        weatherWrapper = WeatherReport()
-        todaysWeather = weatherWrapper.getTodayWeather(location)
-
-        # for i in todaysWeather:
+        pass
 
 if __name__ == '__main__':
     unittest.main()
